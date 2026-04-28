d459e891916cbc4f98d075d253c681f69262dcbc
abh80 <abhb787@outlook.com>
2026-04-28 21:30:00 +0530

    chore: init scala-cli scaffold

---
+++ project.scala
@@ -0,0 +1,7 @@
+//> using scala 3.5.2
+//> using platform scala-js
+//> using jsModuleKind none
+//> using jsHeader "// migrate-to-gitlab — Scala.js client\n"
+//> using dep com.raquo::laminar::17.2.0
+//> using dep com.lihaoyi::upickle::4.0.2
+//> using dep org.scala-js::scalajs-dom::2.8.0
